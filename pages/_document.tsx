bf828955319738feaa36d4c1b53cae97f9610869
JSB-human <tommy63196@gmail.com>
2022-08-08 12:27:47 +0900

    rec

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
         <Html>
         <Head>
-        <script
+        <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer`}
-        />
+        ></Script>
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
